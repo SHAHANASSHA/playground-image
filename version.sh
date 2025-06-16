8a5b91c4ca0e24e2e0a191e001fc9774e357c815
SHAHANASSHA <126233992+SHAHANASSHA@users.noreply.github.com>
2025-06-16 17:51:20 +0530

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+set -e
+
+git fetch --tags
+
+LATEST_TAG=$(git describe --tags --abbrev=0 2>/dev/null || echo "v0.0")
+
+VERSION=${LATEST_TAG#v}
+MAJOR=$(echo $VERSION | cut -d. -f1)
+MINOR=$(echo $VERSION | cut -d. -f2)
+
+COMMIT_MSG=$(git log -1 --pretty=%B)
+
+if echo "$COMMIT_MSG" | grep -q "#major"; then
+    MAJOR=$((MAJOR + 1))
+    MINOR=0
+else
+    MINOR=$((MINOR + 1))
+fi
+
+NEW_TAG="v${MAJOR}.${MINOR}"
+
+echo "New tag: $NEW_TAG"
+
+git tag "$NEW_TAG"
+git push origin "$NEW_TAG"
+
+echo "$NEW_TAG"
 
