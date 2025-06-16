809a57b305ee3b0f2d9a0218ae41f8542aa02a4d
SHAHANASSHA <126233992+SHAHANASSHA@users.noreply.github.com>
2025-06-16 16:49:28 +0530

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-LATEST_TAG=$(git describe --tags --abbrev=0 >/dev/null || echo "0.0") 
+LATEST_TAG=$(git describe --tags --abbrev=0 >/dev/null || echo "v0.0") 
 LATEST_TAG=${LATEST_TAG#v}
-MAJOR=$(echo "$VERSION" | cut -d. -f1)       
-MINOR=$(echo "$VERSION" | cut -d. -f2)       
+MAJOR=$(echo "$LATEST_TAG" | cut -d. -f1)       
+MINOR=$(echo "$LATEST_TAG" | cut -d. -f2)       
 
 COMMIT_MSG=$(git log -1 --pretty=%B)         
 if  echo "$COMMIT_MSG" | grep -q "#major"; then 
@@ -18,11 +18,4 @@ echo "$NEW_VERSION"
 
 git config user.name "SHAHANASSHA"
 git config user.email "shashahanas5@gmail.com"
-git add "$VERSION_FILE"
-if 
-git diff --cached --quiet; then
-        echo "Nothing to commit"
-else
-        git commit -m "Version bump to $NEW_VERSION"
 
-fi
