1dfa46a49bd37074bd7b8c970ca44cf21050eadc
SHAHANASSHA <126233992+SHAHANASSHA@users.noreply.github.com>
2025-06-20 18:32:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ go build -o ./firestarter/firestarter firestarter.go
 
 go build -o ./firestarter/ws-server ws-server.go
 
-docker build -t repo.synnefo.solutions/devaraj/2vmpg .
+docker build -t repo.synnefo.solutions/devaraj/2vmpg . 
